159fbe44383613d76bf70529e9d8efae4c291b16
Kashyap <sky01162003@gamil.com>
2024-03-25 16:45:41 -0400

    chnaged expose to expos

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,10 +3,6 @@ const model = require("../models/event");
 
 //GET /events: send all the events
 exports.index = (req, res, next) => {
-  // res.send('send all the stories');
-  // let events = model.find();
-  // res.render("./event/index", { events });
-
   model
     .find()
     .then((events) => {
@@ -42,18 +38,6 @@ exports.create = (req, res, next) => {
 
 //GET /events/:id
 exports.show = (req, res, next) => {
-  // let id = req.params.id;
-  // let event = model.findById(id);
-  // // res.send('send event with id ' + req.params.id);
-  // if (event) {
-  //   res.render("./event/show", { event });
-  // } else {
-  //   // res.status(404).send('Cannot find event with id: ' + id);
-  //   let err = Error("Cannot find event with id " + id);
-  //   err.status = 404;
-  //   next(err);
-  // }
-
   let id = req.params.id;
   if (!id.match(/^[0-9a-fA-F]{24}$/)) {
     let err = new Error("Invalid event id");
@@ -78,18 +62,6 @@ exports.show = (req, res, next) => {
 
 //GET /events/:id/edit:
 exports.edit = (req, res, next) => {
-  //res.send('send the edit form');
-  // let id = req.params.id;
-  // let event = model.findById(id);
-  // if (event) {
-  //   res.render("./event/edit", { event });
-  // } else {
-  //   // res.status(404).send('Cannot find event with id: ' + id);
-  //   let err = Error("Cannot find event with id " + id);
-  //   err.status = 404;
-  //   next(err);
-  // }
-
   let id = req.params.id;
   if (!id.match(/^[0-9a-fA-F]{24}$/) === null) {
     let err = new Error("Invalid id");
@@ -114,20 +86,6 @@ exports.edit = (req, res, next) => {
 
 //PUT /events/:id
 exports.update = (req, res, next) => {
-  // //res.status(200).send(`update event with id: ${req.params.id}`);
-  // //res.send('update event with id: ', req.params.id);
-  // let event = req.body;
-  // //console.log(event);
-  // let id = req.params.id;
-  // if (model.updateById(id, event)) {
-  //   res.redirect("/events/" + id);
-  // } else {
-  //   // res.status(404).send('Cannot find event with id: ' + id);
-  //   let err = Error("Cannot find event with id " + id);
-  //   err.status = 404;
-  //   next(err);
-  // }
-
   let event = req.body;
   let id = req.params.id;
   if (!id.match(/^[0-9a-fA-F]{24}$/) === null) {
@@ -141,8 +99,8 @@ exports.update = (req, res, next) => {
       runValidators: true,
       useFindAndModify: false,
     })
-    .then((updatedEvent) => {
-      if (updatedEvent) {
+    .then((event) => {
+      if (event) {
         res.redirect("/events/" + id);
       } else {
         let err = Error("Cannot find event with id " + id);
@@ -160,16 +118,6 @@ exports.update = (req, res, next) => {
 
 //DELETE /events/:id
 exports.delete = (req, res, next) => {
-  // //res.send('delete event with id: ', req.params.id);
-  // let id = req.params.id;
-  // if (model.deleteById(id)) {
-  //   res.redirect("/events");
-  // } else {
-  //   // res.status(404).send('Cannot find event with id: ' + id);
-  //   let err = Error("Cannot find event with id " + id);
-  //   err.status = 404;
-  //   next(err);
-  // }
   let id = req.params.id;
   if (!id.match(/^[0-9a-fA-F]{24}$/) === null) {
     let err = new Error("Invalid id");
